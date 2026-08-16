bfb240f5a214787523e0c0d19131aab1b43ea874
Noam Ron <146370694+NoamRon1@users.noreply.github.com>
2026-08-16 18:51:00 +0300

    Feat: updated CMake and main to work with ByteWriter

--- Compiler/main.cpp
+++ Compiler/main.cpp
@@ -4,6 +4,7 @@
 #include "AST/Types/Integer.h"
 #include "AST/ASTNode.h"
 #include "AST/Types/Float.h"
+#include "ByteWriter.h"
 
 int main() {
     Integer int1(1);
@@ -17,6 +18,7 @@ int main() {
 
     std::vector<Byte> vec;
     add2.compile(vec);
+    writeFile("output.bin", vec, {});
 
     for (const Byte& b : vec) {
         std::cout << std::bitset<8>(b) << std::endl;
